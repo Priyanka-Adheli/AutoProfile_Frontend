1c8d7d3ffcfda45d3563a5e536129b2a8ad84327
Priyanka-Adheli <priyankaadheli12@gmail.com>
2026-06-29 14:28:42 +0530

    removed copy right

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -12,19 +12,7 @@ const Footer = () =>{
             
             <footer className="flex flex-col md:flex-row gap-3 items-center justify-around w-full py-4 text-sm text-amber-700 mt-5">
                 <p>Copyright © 2025 AutoProfile. All rights reservered.</p>
-                <div className="flex items-center gap-4">
-                    <a href="#" className="hover:text-white transition-all">
-                        Contact Us
-                    </a>
-                    <div className="h-8 w-px bg-white/20"></div>
-                    <a href="#" className="hover:text-white transition-all">
-                        Privacy Policy
-                    </a>
-                    <div className="h-8 w-px bg-white/20"></div>
-                    <a href="#" className="hover:text-white transition-all">
-                        Trademark Policy
-                    </a>
-                </div>
+        
             </footer>
         </>
     );
